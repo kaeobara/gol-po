ba438eecbe027aa7ba33fe24dcf20c23897004c7
kaeobara <68631676+kaeobara@users.noreply.github.com>
2020-09-25 17:56:45 +0900

    messageモデルの作成

--- app/models/recruit.rb
+++ app/models/recruit.rb
@@ -1,5 +1,6 @@
 class Recruit < ApplicationRecord
   belongs_to :user
   belongs_to :golf
+  has_many :messages
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   has_many :golves
   has_many :recruits
   has_many :comments, dependent: :destroy
+  has_many :messages
 end
