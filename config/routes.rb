a7c36d8ca382506d73794d40e45d9b32a923300b
kaeobara <68631676+kaeobara@users.noreply.github.com>
2020-09-25 17:40:31 +0900

    コメント機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "golves#index"
-  resources :golves
+  resources :golves do
+    resources :recruits
+    resources :comments, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
